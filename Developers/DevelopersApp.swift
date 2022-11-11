2221a3d1dfc5f67d92bc2fe0831433ca70c53b2a
Elijah Patric <foreverelijah@gmail.com>
2022-11-11 16:24:19 -0600

    Initial Commit

---
+++ Developers/DevelopersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevelopersApp.swift
+//  Developers
+//
+//  Created by Elijah on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct DevelopersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
